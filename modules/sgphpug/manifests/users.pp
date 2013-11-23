d7b368e20b05e198945443cae7b60521a80aa383
michael.cheng <michael.cheng@mig33global.com>
2013-11-23 23:30:24 +0800

    Add user group as well.

--- modules/sgphpug/manifests/users.pp
+++ modules/sgphpug/manifests/users.pp
@@ -1,9 +1,23 @@
 class sgphpug::users
 {
+	group{
+		'laurence':
+			ensure => present
+	}
+
 	user {
 		'laurence':
 			ensure => present,
-			groups => ['apache', 'wheel'],
-			home => '/home/laurence'
+			groups => ['laurence', 'apache', 'wheel'],
+			home => '/home/laurence',
+			require => [ Group['laurence'], File['/home/laurence'] ]
+	}
+
+	file {
+		"/home/laurence":
+			ensure => directory,
+			owner => 'laurence',
+			group => 'laurence'
+			require => [ Group['laurence'] ]
 	}
 }
