3fb9098dd44951ad6c7bef15ea96ce5e417beae7
michael.cheng <michael.cheng@mig33global.com>
2013-11-24 19:54:47 +0800

    Change to defined type for user.

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,4 +6,13 @@ node 'sgphpug.cloudapp.net'{
     	root_pwd => 'media1'
 	}
 	class { "sgphpug::users": }
+
+	sgphpug::user{ "miccheng":
+		public_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAxRRDYFyVZ9oJDag2FBuxL9MzSVXSzJh/vT8KSEF6KYUvjPW257lK+iIWeK0x5yAHZ1OetJ9s0nYZRIfkLkf1HlJMrRRYgaZRKZuzlqmE00ozP6LHPSW4DaxPT+NElnUYoznlgCCM9A+A4ZNdOUgBHXc44qY0QdmDRQBTgS5JLEoSafa6o3MU3gvTPwK7POVxFtm+pmfix4OOxqNN0S/qoHMCfKwrp4737CEpJgqE6cgopNkG9pnFC90ZgYK/bbpD24KKRNf/aXhJsKEU+81c4WL88P6vZR3NIioNhqjgpqFFryXNc6P0F47XpFjdz9L2kvu4GuVQxEeHyV78ozxrfw== miccheng@miccheng-laptop'
+	}
+	sgphpug::user{ "sam":
+		public_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmKHXneslzeZQmueuMALCM5WXii2uxlU5cb+TxeQ0aTJftWM4sD2AXXgQ7Jhtf3X/byhdrVsbWyB5pZ3NcfDWUEBnQWsZ3BVahapGed0Knxxbf0201NBwZ/ViisTMyCKmH6+Eb4k4j8Ox44ItkZXeg3L5seWx3/ibjjeBWCxijsqiPCSEghKtmoTmXNC4aaGmyNk3eabcRNkH0uViaH4mDukwapynqRreRh7Hb35aTotPRZkm2cfq2DnvkUIDgx8ncksmKfHJBPZXWuLTw2AZBC152QAd8HkO9DCSvUrZnJkv6Qvy5nzacFekqIpwg58nqcCDbn2nLzB7sMcQSueWow== puppy'
+	}
+	sgphpug::user{ "uzyn": }
+	sgphpug::user{ "laurence": }
 }
---
+++ modules/sgphpug/manifests/user.pp
@@ -0,0 +1,40 @@
+define sgphpug::user(
+	$public_key = ''
+)
+{
+	group{
+		$title:
+			ensure => present
+	}
+
+	user {
+		$title:
+			ensure => present,
+			groups => [$title, 'apache', 'wheel'],
+			home => "/home/$title",
+			require => [ Group[$title] ]
+			;
+	}
+
+	file {
+		"/home/$title":
+			ensure => directory,
+			owner => $title,
+			group => $title,
+			require => [ Group[$title] ]
+			;
+		"/home/$title/.ssh/":
+			ensure => directory,
+			owner => $title,
+			group => $title,
+			require => [ Group[$title] ]
+			;
+		"/home/$title/.ssh/authorized_keys":
+			ensure => present,
+			content => $public_key,
+			owner => $title,
+			group => $title,
+			require => [ File["/home/$title/.ssh/"], Group[$title] ]
+			;
+	}
+}
