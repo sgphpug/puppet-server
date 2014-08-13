945e7bd5e89e47d1d83e5559a4530df22516ce38
Michael Cheng <mcheng.work@gmail.com>
2014-08-13 19:49:05 +0800

    Added Robert's Public key.

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,6 +12,9 @@ node 'sgphpug.cloudapp.net'{
 	sgphpug::user{ "sam":
 		public_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmKHXneslzeZQmueuMALCM5WXii2uxlU5cb+TxeQ0aTJftWM4sD2AXXgQ7Jhtf3X/byhdrVsbWyB5pZ3NcfDWUEBnQWsZ3BVahapGed0Knxxbf0201NBwZ/ViisTMyCKmH6+Eb4k4j8Ox44ItkZXeg3L5seWx3/ibjjeBWCxijsqiPCSEghKtmoTmXNC4aaGmyNk3eabcRNkH0uViaH4mDukwapynqRreRh7Hb35aTotPRZkm2cfq2DnvkUIDgx8ncksmKfHJBPZXWuLTw2AZBC152QAd8HkO9DCSvUrZnJkv6Qvy5nzacFekqIpwg58nqcCDbn2nLzB7sMcQSueWow== puppy'
 	}
+  sgphpug::user{ "robert":
+    public_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCY92bXCvJaTznqARjF38y3zpXLT0cvfvq0hCL4vxAW4EKN0KgKSdqnbFUXZZr3Trc2FnO8+GOpJV3mUbyJTPvx9njtc3yQq1Wg14VFNUUSeyGpTL4c8oHgc22h2QP7+HoomK77Y22Z5YorgGLcS6LbGDKu16983/b5/47uBoTvYynddSp3tRUfMfU0jLiS99SsnJnxdQeJ01krug++qi2Ji72mxe4KoCZvWFb6EqofjRwGL/b8cYgA/Zj4AzYrh6E9GxMudewkoSjLRIwt5+ZZKqbC+PKoaRH0pWukrQ7cOeqnSsiJOpvtG7lKTCzcDhna9uZzQ3xFLonr2NYKIs6V contact@robertsky.com'
+  }
 	sgphpug::user{ "uzyn": }
 	sgphpug::user{ "laurence": }
 }
